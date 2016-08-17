f1d383c2d481fe56b97a6a70735bfd1806152f64
adamjast <adamjast@mac.com>
2016-08-17 17:41:56 -0400

    Create tip-jar.ino

---
+++ built-with/ultra-sonic/arduino-uno/arduino_code/tip-jar/tip-jar.ino
@@ -0,0 +1,71 @@
+/*
+ HC-SR04 Ping distance sensor:
+ VCC to arduino 5v 
+ GND to arduino GND
+ Echo to Arduino pin 7 
+ Trig to Arduino pin 8
+ 
+ This sketch originates from Virtualmix: http://goo.gl/kJ8Gl
+ Has been modified by Winkle ink here: http://winkleink.blogspot.com.au/2012/05/arduino-hc-sr04-ultrasonic-distance.html
+ Modified further by ScottC here: http://arduinobasics.blogspot.com.au/2012/11/arduinobasics-hc-sr04-ultrasonic-sensor.html
+ on 10 Nov 2012.
+ Modified for tip-jar by Adam B. WIOT westboroughiot.com
+ */
+
+#define echoPin 7 // Echo Pin
+#define trigPin 8 // Trigger Pin
+#define LEDPin 13 // Onboard LED
+
+int maximumRange = 200; // Maximum range needed
+int minimumRange = 0; // Minimum range needed
+long duration, distance; // Duration used to calculate distance
+int dis_without_tip; // sets the distance without a tip
+int calibrations[10];
+
+void setup() {
+ Serial.begin (9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+ pinMode(LEDPin, OUTPUT); // Use LED indicator (if required)
+
+}
+
+void loop() {
+/* The following trigPin/echoPin cycle is used to determine the
+ distance of the nearest object by bouncing soundwaves off of it. */ 
+ digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
+
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+
+ //Calculate the distance (in cm) based on the speed of sound.
+ distance = duration/58.2;
+
+ // start calibration for no tip
+ // set it with the first found duration
+  if (!dis_without_tip) {
+     Serial.println("Setting non tip distance to ");
+      Serial.println(distance);
+    dis_without_tip = distance;
+  }
+
+// for debugging show the current distance
+// Serial.println(distance);
+ 
+ if ( distance < dis_without_tip){
+//  
+ Serial.println("tip");
+ digitalWrite(LEDPin, HIGH);
+ 
+ } else {
+//  Serial.println("no tip");
+ digitalWrite(LEDPin, LOW); 
+ }
+ 
+ //Delay 50ms before next reading.
+ delay(50);
+}
